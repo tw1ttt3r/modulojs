10a6e60af0a558931beeb3f658af3dc937ed9e0c
tw1ttt3r <tw1ttt3r@gmail.com>
2021-11-27 15:03:18 -0600

    programacion fucional

--- teoria/main.js
+++
@@ -1,7 +0,0 @@
-const antonio = new Persona('Antonio', 30,'M','Latino','negro','café','programador');
-
-console.log(antonio);
-
-const antonioProgramador = new Programador('Antonio', 30,'M','Latino','negro','café','programador', ['javascript', 'java'], 10, ['web', 'desktop'], ['microsoft', 'MND', 'oracle'])
-
-console.log(antonioProgramador);
--- teoria/programador.js
+++
@@ -1,9 +0,0 @@
-class Programador extends Persona {
-    constructor(nombre, edad, genero, raza, colorCabello, colorOjos, ocupacion, lenguaje, experiencia, plataforma, certificaciones) {
-        super(nombre, edad, genero, raza, colorCabello, colorOjos, ocupacion);
-        this.lenguaje = lenguaje;
-        this.experiencia = experiencia;
-        this.plataforma = plataforma;
-        this.certificaciones = certificaciones;
-    }
-}
