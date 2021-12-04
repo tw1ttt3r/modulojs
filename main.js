8702c461383a36f662e856e3f8a51db4418ec019
tw1ttt3r <tw1ttt3r@gmail.com>
2021-12-04 12:32:09 -0600

    uso de append y appendChild

--- main.js
+++ main.js
@@ -1,33 +1,126 @@
-// Selecciones de nodos a partir de sus atributos
+// // Selecciones de nodos a partir de sus atributos
 
-// ID
-// getElementByID(<string del ID>)
-// querySelector(<string del selector de ID>)
+// // ID
+// // getElementByID(<string del ID>)
+// // querySelector(<string del selector de ID>)
 
-const sectionPrincipal = document.querySelector('#wrapper');
+// const sectionPrincipal = document.querySelector('#wrapper');
 
-console.log(sectionPrincipal);
+// console.log(sectionPrincipal);
 
 
-// CLASES
-// getElementsByClassName(<string de la CLASE>)
-// querySelectorAll(<string del selector de la CLASE>)
-const allContainers = document.querySelectorAll('.container');
+// // CLASES
+// // getElementsByClassName(<string de la CLASE>)
+// // querySelectorAll(<string del selector de la CLASE>)
+// const allContainers = document.querySelectorAll('.container');
 
-console.log(allContainers);
+// console.log(allContainers);
 
-const nuevoArreglo = Array.from(allContainers);
+// const nuevoArreglo = Array.from(allContainers);
 
-console.log(nuevoArreglo)
+// console.log(nuevoArreglo)
 
-// Obtener solo los elementos que tengan imagen
+// // Obtener solo los elementos que tengan imagen
 
-const soloImagenes = nuevoArreglo.filter( (value, index, array) => {
-    if (value.children.length > 0) {
-        if (value.children[0].tagName === 'IMG'){
-            return value
-        }
-    }
-});
+// const soloImagenes = nuevoArreglo.filter( (value, index, array) => {
+//     if (value.children.length > 0) {
+//         if (value.children[0].tagName === 'IMG'){
+//             return value
+//         }
+//     }
+// });
 
-console.log(soloImagenes)
+// console.log(soloImagenes)
+
+// // TAGNAME
+// // getElementsByTagName(<string del nombre de la tagname>)
+// // querySelectorAll(<string del nombre de la tagname>)
+
+// // seleccionar todos los div
+
+// const allDivs = document.querySelectorAll('div');
+
+// console.log(allDivs)
+
+
+// Crear nodos
+
+// document.createElement(<string de mi tagname>)
+
+const nuevoBoton = document.createElement('button');
+nuevoBoton.textContent = 'Soy un nuevo boton';
+
+// agregar nuevos elementos o nodos
+
+// sectionPrincipal.append(nuevoBoton);
+
+// sectionPrincipal.firstChild.nextSibling.append(nuevoBoton)
+
+// sectionPrincipal.children[1].append(nuevoBoton)
+
+
+// crear elemento complejos
+
+// const wrapperComponente = document.createElement('div');
+// const divisionImg = document.createElement('div');
+// const divImage = document.createElement('img');
+// divImage.src = 'https://i.pinimg.com/736x/0b/02/f8/0b02f855419cfbd36e7249b275bc955c.jpg';
+// divImage.alt = 'foto de usuario';
+// divImage.classList.add('profileImage');
+// divisionImg.appendChild(divImage);
+// const divisionName = document.createElement('div')
+// const nameUser = document.createTextNode('Maria');
+// divisionName.appendChild(nameUser);
+// wrapperComponente.appendChild(divisionImg)
+// wrapperComponente.appendChild(divisionName)
+
+// sectionPrincipal.children[0].append(wrapperComponente);
+
+// sectionPrincipal.children[0].classList.remove('container');
+
+
+const wrapperCard = document.createElement('div');
+wrapperCard.classList.add('cardStyle');
+
+const wrapperImage = document.createElement('div');
+
+const imageProfile = document.createElement('img');
+imageProfile.src = 'https://i.pinimg.com/736x/0b/02/f8/0b02f855419cfbd36e7249b275bc955c.jpg';
+imageProfile.alt = 'foto de usuario';
+imageProfile.classList.add('profileImage');
+wrapperImage.appendChild(imageProfile);
+wrapperCard.appendChild(wrapperImage);
+
+const wrapperName = document.createElement('div');
+const nameProfile = document.createElement('p');
+nameProfile.textContent = 'Maria Hernandez';
+wrapperName.appendChild(nameProfile);
+wrapperCard.appendChild(wrapperName);
+
+
+const wrapperBio = document.createElement('div');
+wrapperBio.classList.add('profileBio');
+const bioProfile = document.createElement('p');
+bioProfile.textContent = `
+
+Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?
+
+My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?
+
+Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?
+
+`;
+wrapperBio.appendChild(bioProfile);
+wrapperCard.appendChild(wrapperBio);
+
+const wrapperActions = document.createElement('div');
+wrapperActions.classList.add('actionsStyle');
+const action1 = document.createElement('button');
+action1.textContent = 'X'
+const action2 = document.createElement('button');
+action2.textContent = 'A'
+wrapperActions.appendChild(action1);
+wrapperActions.appendChild(action2);
+wrapperCard.appendChild(wrapperActions);
+
+document.body.append(wrapperCard);
