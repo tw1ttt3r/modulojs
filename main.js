596aec390961543b0457c3be3f0fbea379f09969
tw1ttt3r <tw1ttt3r@gmail.com>
2021-12-04 13:40:16 -0600

    uso de listeners

--- main.js
+++ main.js
@@ -79,6 +79,29 @@ nuevoBoton.textContent = 'Soy un nuevo boton';
 // sectionPrincipal.children[0].classList.remove('container');
 
 
+function changeAction(e) {
+    console.log(e)
+    const childs = Array.from(e.target.parentNode.previousSibling.children).reduce((previous, current) => {
+        previous[current.tagName.toLowerCase()] = current;
+        return previous;
+    }, {});
+    if (e.target.textContent === 'editar') {
+        e.target.nextSibling.disabled = false;
+        const editBio = childs.p.textContent;
+        childs.textarea.value = editBio;
+        childs.textarea.style.display = 'block';
+        childs.p.style.display = 'none';
+    } else {
+        e.target.previousSibling.disabled = false;
+        const newBio = childs.textarea.value;
+        childs.textarea.style.display = 'none';
+        childs.p.textContent = newBio;
+        childs.p.style.display = 'block';
+        childs.textarea.value = '';
+    }
+    e.target.disabled = true;
+}
+
 const wrapperCard = document.createElement('div');
 wrapperCard.classList.add('cardStyle');
 
@@ -101,24 +124,30 @@ wrapperCard.appendChild(wrapperName);
 const wrapperBio = document.createElement('div');
 wrapperBio.classList.add('profileBio');
 const bioProfile = document.createElement('p');
-bioProfile.textContent = `
-
-Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?
+const bio = `Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?
 
 My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?
 
 Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'?
 
 `;
+bioProfile.textContent = bio;
+const bioEdit = document.createElement('textarea');
+bioEdit.style.display = 'none';
+bioEdit.value = bio;
+wrapperBio.appendChild(bioEdit);
 wrapperBio.appendChild(bioProfile);
 wrapperCard.appendChild(wrapperBio);
 
 const wrapperActions = document.createElement('div');
 wrapperActions.classList.add('actionsStyle');
 const action1 = document.createElement('button');
-action1.textContent = 'X'
+action1.textContent = 'editar'
+action1.addEventListener('click', changeAction)
 const action2 = document.createElement('button');
-action2.textContent = 'A'
+action2.textContent = 'guardar'
+action2.addEventListener('click', changeAction)
+action2.disabled = true
 wrapperActions.appendChild(action1);
 wrapperActions.appendChild(action2);
 wrapperCard.appendChild(wrapperActions);
